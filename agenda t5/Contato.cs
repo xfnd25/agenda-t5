0757d564e049a3dc11acb5629467e361140d06ea
xfnd25 <118480043+xfnd25@users.noreply.github.com>
2023-05-04 18:11:55 -0300

    criada classe contato.cs
    foi criada a classe contato.cs

---
+++ agenda t5/Contato.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace agenda_t5
+{
+    internal class Contato
+    {
+        private string primeiroNome;
+        private string sobrenome;
+        private string telefone;
+
+        public string PrimeiroNome
+        {
+            get { return primeiroNome; }
+            set { primeiroNome = value; }
+        }
+
+        public string Sobrenome
+        {
+            get { return sobrenome; }
+            set { sobrenome = value; }
+        }
+
+        public string Telefone
+        {
+          get { return telefone; }
+            set
+            {
+                if (value.Length == 11)
+                {
+                    telefone = value;
+                }
+                else
+                {
+                    telefone = "00-00000-0000";
+                }
+            }                       
+        }   
+
+        public Contato() 
+        {
+            PrimeiroNome = "João";
+            Sobrenome = "Da Silva";
+            Telefone = "11-98888-8776";
+        }
+        public Contato(string primeiroNome, string sobrenome, string telefone)
+        {
+            PrimeiroNome = primeiroNome;
+            Sobrenome = sobrenome;
+            Telefone = telefone;
+        }
+
+        public override string ToString()
+        {
+            string saida = String.Empty;
+            saida += String.Format("{0}, {1}", PrimeiroNome, Sobrenome);
+            saida += String.Format("{0}-{1}-{2}", Telefone.Substring(0,1), Telefone.Substring(2,6), Telefone.Substring(7,10));
+        }
+
+    }
+}
